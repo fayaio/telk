c17e83371264209502088977eb942e347601ac0e
seyi4mi <seyi4mi99@gmail.com>
2022-03-24 01:11:30 +0800

    first commit

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export {default as NavBar} from "./NavBar"
-export {default as Welcome} from "./Welcome"
-export {default as Features} from "./features"
-export {default as Team} from "./Team"
-export {default as Footer} from "./Footer"
-export {default as Testimonial} from "./Testimonial"
-export {default as TeamMembers} from "./TeamMembers"
