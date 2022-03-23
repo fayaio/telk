15528a440bda3cfc3a0c3fc89f04ee7e2a65ce82
seyi4mi <seyi4mi99@gmail.com>
2022-03-24 05:16:21 +0800

    styled navbar

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,23 +1,24 @@
-import Link from 'next/link'
-import Image from 'next/image'
-
+import Link from "next/link";
+import Image from "next/image";
 
 const Nav = () => {
-    return ( 
-        <nav>
-            <div className="logo">
-               <Image src="/bjflogo.svg" width={128} height={77}/>
-            </div>
+  return (
+    <nav>
+      <Link href="/gallery">
+        <a>Gallery</a>
+      </Link>
+      <Link href="/properties">
+        <a>Properties</a>
+      </Link>
+      <Image src="/bjflogo.svg" width={120} height={77} />
+      <Link href="/about">
+        <a>About Us</a>
+      </Link>
+      <Link href="/contact">
+        <a>Contact Us</a>
+      </Link>
+    </nav>
+  );
+};
 
-            <Link href="/gallery"><a>Gallery</a></Link>
-            <Link href="/properties"><a>Properties</a></Link>
-            
-            <Link href="/about"><a>About Us</a></Link>
-            <Link href="/contact" ><a>Contact Us</a></Link>
-            
-
-        </nav>
-     );
-}
- 
 export default Nav;
