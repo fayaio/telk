6d07164c7fa05ef97f0a96a942060523d5f00324
seyi4mi <seyi4mi99@gmail.com>
2022-03-24 03:33:13 +0800

    made layout file inside compontenrs folder and wrap it arount the children

---
+++ frontend/components/Layout.js
@@ -0,0 +1,15 @@
+import Nav from './Navbar'
+import Footer from  './Footer'
+
+const Layout = ({children}) => {
+    return ( 
+        <div className="content">
+            <Nav />
+            { children }
+            <Footer />
+
+        </div>
+     );
+}
+ 
+export default Layout;
---
+++ frontend/components/footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div>
+            <p>Copyright 2022 BJF Properties</p>
+        </div>
+     );
+}
+ 
+export default Footer;
