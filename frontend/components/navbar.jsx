6785054d08ab9d8169b8ac7638db90b58b5a290f
seyi4mi <seyi4mi99@gmail.com>
2022-03-24 02:58:07 +0800

    added compontents folder, .babelrc and edit .eslintrc.json and index.js

---
+++ frontend/components/navbar.jsx
@@ -0,0 +1,18 @@
+const Nav = () => {
+    return ( 
+        <nav>
+            <div className="logo">
+                <h1>BJF Properties</h1>
+            </div>
+
+            <a>Gallery</a>
+            <a>Properties</a>
+            <a>About Us</a>
+            <a>Contact Us</a>
+            
+
+        </nav>
+     );
+}
+ 
+export default Nav;
