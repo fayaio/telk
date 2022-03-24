50734899e0a5397c2fca663cddefe5be59ef4dbe
seyi4mi <seyi4mi99@gmail.com>
2022-03-24 10:47:44 +0800

    added testimonials section in index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,34 +1,36 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { library } from '@fortawesome/fontawesome-svg-core';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCode, faHighlighter } from '@fortawesome/free-solid-svg-icons';
-
+import { faCode, faHighlighter } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
-   <>
+    <>
       <Head>
         <title>BJF Properties | Home</title>
         <meta name="keywords" content="BJF Properties" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        {/* Banner Section */}
+      {/* Banner Section */}
       <div className={styles.bannerContainer}>
         <h3 className={styles.bannerText}> YOU DREAM IT, WE BUILD IT</h3>
       </div>
 
-        {/* Explore Section */}
-        <div className={styles.exploreContainer}>
+      {/* Explore Section */}
+      <div className={styles.exploreContainer}>
         <h5 className={styles.exploreText}> Explore Properties In Nigeria</h5>
-        <div  className={styles.exploreContent}></div>
+        <div className={styles.exploreContent}></div>
+      </div>
+
+      {/* Testimonials Section */}
+      <div className={styles.testimonialContainer}>
+      <div className={styles.HeaderTexts}>
+        <h5 className={styles.testimonialHeaderText}> Testimonials</h5>
+        <p className={styles.testimonialSubHeaderText}> What our customers say about us</p>
         </div>
-
-
-
-
-
+      </div>
     </>
-  )
+  );
 }
