84723b4de100b70729705b5b5adb50ab8f536efb
seyi4mi <seyi4mi99@gmail.com>
2022-03-24 10:05:43 +0800

    made banner responsive

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,23 +1,36 @@
 import Link from "next/link";
 import Image from "next/image";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
   return (
-    <nav>
-      <Link href="/gallery">
-        <a>Gallery</a>
-      </Link>
-      <Link href="/properties">
-        <a>Properties</a>
-      </Link>
-      <Image src="/bjflogo.svg" width={120} height={77} />
-      <Link href="/about">
-        <a>About Us</a>
-      </Link>
-      <Link href="/contact">
-        <a>Contact Us</a>
-      </Link>
-    </nav>
+    
+    <>
+
+   
+      <nav>
+        <FontAwesomeIcon className="nav-close" icon={faXmark}></FontAwesomeIcon>
+        <Link href="/gallery">
+          <a>Gallery</a>
+        </Link>
+        <Link href="/properties">
+          <a>Properties</a>
+        </Link>
+        <Image className="logo" src="/bjflogo.svg" width={120} height={77} />
+        <Link href="/about">
+          <a>About Us</a>
+        </Link>
+        <Link href="/contact">
+          <a>Contact Us</a>
+        </Link>
+
+        <FontAwesomeIcon className="nav-toggle" icon={faBars}></FontAwesomeIcon>
+      </nav>
+    
+      </>
+    
   );
 };
 
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,15 +1,23 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode, faHighlighter } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Home() {
   return (
    <>
       <Head>
         <title>BJF Properties | Home</title>
-
         <meta name="keywords" content="BJF Properties" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+        {/* Banner Section */}
+      <div className={styles.bannerContainer}>
+        <h3 className={styles.bannerText}> YOU DREAM IT, WE BUILD IT</h3>
+      </div>
 
 
 
