594af0a5848fd3813c8029b5b83f66e2df4c9347
Faithful <faithful.anere@sda.se>
2022-04-07 19:28:33 +0200

    made cm demo fully responsive (#5)
    Co-authored-by: seyi4mi <seyi4mi99@gmail.com>

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,6 +4,7 @@ import styles from "../styles/Home.module.css";
 import React from "react";
 
 export default function Home() {
+  
   return (
     <>
       <Head>
@@ -23,8 +24,8 @@ export default function Home() {
           </div>
         </div>
 
-        {/* Explore Section */}
 
+        {/* Explore Section */}
         <div className={styles.explore}>
           <h2 className={styles.exploreheader}>
             Explore Properties In Nigeria
@@ -76,110 +77,74 @@ export default function Home() {
           </ul>
         </div>
 
-   {/* Certificate Section */}
-   <div className={styles.cert}>
+
+        {/* Certificate Section */}
+        <div className={styles.cert}>
           <h5 className={styles.certHeader}> We Are Certified </h5>
           <div className={styles.certimg}>
             <Image src="/certificate.svg" alt="" width={500} height={500} />
           </div>
         </div>
 
+        <h3 className={styles.teamHeader}> Meet The Team </h3>
 
-
-        <div className={styles.teamSection}>
-				<div className={styles.teamContainer}>
-					<h3 className={styles.teamHeader}> Meet The Team </h3>
-					<div className={styles.allTeams}>
-						<div className={styles.team}>
-							<div className={styles.memeberFrame}>
-								<img className={styles.memeberImg} src="/CEO.svg" alt="" />
-								<a href="https://www.google.com/">
-									<img className={styles.linkden} src="/Linkedin.svg" alt="" />
-								</a>
-							</div>
-							<div className={styles.teamInfo}>
-								<h4 className={styles.memberName}> Mr Samuel Jegede </h4>
-								<p className={styles.memberRole}> Executive Chairman </p>
-							</div>
-						</div>
-						<div className={styles.team}>
-							<div className={styles.memeberFrame}>
-								<img className={styles.memeberImg} src="/Rufus.svg" alt="" />
-								<a href="https://www.google.com/">
-									<img className={styles.linkden} src="/Linkedin.svg" alt="" />
-								</a>
-							</div>
-							<div className={styles.teamInfo}>
-								<h4 className={styles.memberName}> Mr Rufus Idowu </h4>
-								<p className={styles.memberRole}> General Manager </p>
-							</div>
-						</div>
-						<div className={styles.team}>
-							<div className={styles.memeberFrame}>
-								<img className={styles.memeberImg} src="/Ann.svg" alt="" />
-								<a href="https://www.google.com/">
-									<img className={styles.linkden} src="/Linkedin.svg" alt="" />
-								</a>
-							</div>
-							<div className={styles.teamInfo}>
-								<h4 className={styles.memberName}> Ann Ohaegbue </h4>
-								<p className={styles.memberRole}> Executive Secretary </p>
-							</div>
-						</div>
-						<div className={styles.team}>
-							<div className={styles.memeberFrame}>
-								<img className={styles.memeberImg} src="/Mark.svg" alt="" />
-								<a href="https://www.google.com/">
-									<img className={styles.linkden} src="/Linkedin.svg" alt="" />
-								</a>
-							</div>
-							<div className={styles.teamInfo}>
-								<h4 className={styles.memberName}> Mark Aliu </h4>
-								<p className={styles.memberRole}> Legal Adviser </p>
-							</div>
-						</div>
-						
-			
-					</div>
-				</div>
-			</div>
-
-
-
-
-
-
-
-
-
-
-
-
-        {/* Chairman Section */}
-        {/* <section className={styles.cmcon}>
-          <div className={styles.cm}>
-            <div className={styles.main}>
-              <img src="./chairmanpic.svg" alt="" />
-              <div className={styles.cmContent}>
-                <h1 className={styles.cmHeader}>From The Chairman</h1>
-                <p className={styles.cmText}>
+        <div className={styles.cmsection}>
+        <div className={styles.cm}>
+          <div className={styles.cmimage}  >
+        <img src="./chairmanpic.svg" alt="" width={300} height={390} priority />
+         </div>
+         <div className={styles.cmContent}>
+                  <h2 className={styles.cmHeader}>From The Chairman</h2>
+                  <p className={styles.cmText}>
                   While growing up, one of my dreams has been to build a company
                   that offers trust-based services. Thus, further down the
                   years, I developed an interest in the property development
                   business. This interest was primarily due to my family's
                   acquisition of some landed properties in Ajah, Lagos State,
-                  Nigeria. Indeed, my family was one of those to own land in the
-                  then developing area of Lagos State in the early 1990s.
-                </p>
-                <div className={styles.cmAnchor} type="button">
-                  <a href="/about"> Read More</a>
+                  Nigeria.
+                  </p>
+                  <a className={styles.cmAnchor} href="/about"> Read More</a>
+                </div> 
+          </div>
+        </div>
+   
+        {/* Team Section */}
+        <div className={styles.teamSection}>
+          <div className={styles.teamContainer}>
+           
+            <div className={styles.allTeams}>
+              <div className={styles.team}>
+                <div className={styles.memeberFrame}>
+                  <img className={styles.memeberImg} src="/Rufus.svg" alt="" priority/>
+                </div>
+                <div className={styles.teamInfo}>
+                  <h4 className={styles.memberName}> Mr Rufus Idowu </h4>
+                  <p className={styles.memberRole}> General Manager </p>
+                </div>
+              </div>
+              <div className={styles.team}>
+                <div className={styles.memeberFrame}>
+                  <img className={styles.memeberImg} src="/Ann.svg" alt="" priority/>
+                </div>
+                <div className={styles.teamInfo}>
+                  <h4 className={styles.memberName}> Ann Ohaegbue </h4>
+                  <p className={styles.memberRole}> Executive Secretary </p>
+                </div>
+              </div>
+              <div className={styles.team}>
+                <div className={styles.memeberFrame}>
+                  <img className={styles.memeberImg} src="/Mark.svg" alt="" priority/>
+                </div>
+                <div className={styles.teamInfo}>
+                  <h4 className={styles.memberName}> Mark Aliu </h4>
+                  <p className={styles.memberRole}> Legal Adviser </p>
                 </div>
               </div>
             </div>
           </div>
-        </section> */}
+        </div>
 
-     
+       
       </div>
     </>
   );
