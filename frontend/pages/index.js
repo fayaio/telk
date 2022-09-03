79f039b7fc5efcb85876ef61470c3b86f734893f
seyi4mi <seyi4mi99@gmail.com>
2022-09-03 11:12:31 +0800

    implemented material ui into some of the explore styling

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -20,8 +20,22 @@ import {
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@mui/material/Box';
+
+const useStyles = makeStyles({});
+
 export default function Home() {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const classes = useStyles();
   return (
     <>
       <Head>
@@ -47,11 +61,131 @@ export default function Home() {
             Explore Properties In Nigeria
           </h3>
 
-          <ul className={styles.cards}>
+          <Grid container spacing={6} justifyContent="center">
+            <Grid item>
+              <Card sx={{ maxWidth: 500 }} className={styles.egrid}>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image="/estatecollage.svg"
+                />
+                <CardContent>
+                  {/* <Box component="div" className={styles.card__overlay}>
+                    <Box component="div" className={styles.card__header}></Box>
+                  </Box> */}
+                  <img
+                    src="/bjflog.svg"
+                    alt=""
+                    className={styles.card__thumb}
+                  />
+                  <Box component="div" className={styles.card__headertext}>
+                    <h3 className={styles.card__title}>
+                      MINI ESTATE <span>FOR SALE</span>
+                    </h3>
+
+                    <h5 className={styles.card__estatedetails}>
+                      Housing 5Bungalows with 5numbers of 3bedroom 3 numbers of
+                      <br></br> 2bedroom and 1 Room self-contain with a Security
+                      house
+                    </h5>
+
+                    <li className={styles.card__status}>
+                      <h4>
+                        Title: <span>C OF O</span>
+                      </h4>
+                      <h4>
+                        Size: <span>1287.6245sqms</span>
+                      </h4>
+                      <h4>
+                        Property Location: <span>Ait Area, Alagbado</span>
+                      </h4>
+                    </li>
+                    <Box component="div" className={styles.asskingp__section}>
+                      <Box
+                        component="div"
+                        className={styles.asskingpline1}
+                      ></Box>
+                      <h2 className={styles.asskingp}>
+                        ASKING PRICE: <span>150M</span>
+                      </h2>
+                      <Box
+                        component="div"
+                        className={styles.asskingpline2}
+                      ></Box>
+                    </Box>
+                  </Box>
+                  <a className={styles.moredetails} href="/contact">
+                    Give Us A Call
+                  </a>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item>
+              <Card sx={{ maxWidth: 500 }} className={styles.egrid}>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image="/factorycollage.svg"
+                />
+                <CardContent>
+                  <img
+                    src="/bjflog.svg"
+                    alt=""
+                    className={styles.card__thumb}
+                  />
+                  <Box component="div" className={styles.card__headertext}>
+                    <h3 className={styles.card__title}>
+                      FACTORY <span>FOR SALE</span>
+                    </h3>
+
+                    <h5 className={styles.card__estatedetails2}>
+                      Housing 5Bungalows with 5numbers of 3bedroom 3 numbers of
+                      <br></br> 2bedroom and 1 Room self-contain with a Security
+                      house
+                    </h5>
+                    <li className={styles.card__status}>
+                      <h4>
+                        Title: <span>C OF O</span>
+                      </h4>
+
+                      <h4>
+                        Size: <span>1287.6245sqms</span>
+                      </h4>
+                      <h4 className={styles.extra__padding}>
+                        Property Location:{' '}
+                        <span>Agege, Ifako/Ijaiye Area Lagos</span>
+                      </h4>
+                    </li>
+                    <Box component="div" className={styles.asskingp__section}>
+                      <Box
+                        component="div"
+                        className={styles.asskingpline1}
+                      ></Box>
+                      <h2 className={styles.asskingp}>
+                        ASKING PRICE: <span>300M</span>
+                      </h2>
+                      <Box
+                        component="div"
+                        className={styles.asskingpline2}
+                      ></Box>
+                    </Box>
+                  </Box>
+                  <a className={styles.moredetails} href="/contact">
+                    Give Us A Call
+                  </a>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+
+          {/* <ul className={styles.cards}>
             <li>
               <div href="" className={styles.card}>
-                <img
-                  src="./estatecollage.svg"
+                <Image
+                  width={990}
+                  height={850}
+                  src="/estatecollage.svg"
                   alt=""
                   className={styles.card__image}
                 />
@@ -69,9 +203,9 @@ export default function Home() {
                           MINI ESTATE <span>FOR SALE</span>
                         </h3>
                         <h5 className={styles.card__estatedetails}>
-                          Housing 5Bungalows with 5numbers of 3bedroom
-                          <br></br>3 numbers of 2bedroom and 1 Room self-contain
-                          with a <br></br>Security house
+                          Housing 5Bungalows with 5numbers of 3bedroom 3 numbers
+                          of<br></br> 2bedroom and 1 Room self-contain with a
+                          Security house
                         </h5>
                         <li className={styles.card__status}>
                           <h4>
@@ -94,15 +228,14 @@ export default function Home() {
                       </div>
                     </div>
 
-                    <a className={styles.moredetails} href="/contact">
+                    <a className={styles.moredetails} href="/">
                       {' '}
-                      GIVE US A CALL
+                      More Details
                     </a>
                   </div>
                 </div>
               </div>
             </li>
-
             <li>
               <div href="" className={styles.card}>
                 <img
@@ -146,7 +279,7 @@ export default function Home() {
                       </div>
                     </div>
 
-                    <a className={styles.moredetails} href="/contact">
+                    <a className={styles.moredetails} href="/">
                       {' '}
                       GIVE US A CALL
                     </a>
@@ -154,7 +287,7 @@ export default function Home() {
                 </div>
               </div>
             </li>
-          </ul>
+          </ul> */}
         </div>
 
         {/* Who We Are */}
