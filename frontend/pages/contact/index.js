05312bbc954d1c4e414e4415c055d0c53b5c9a18
seyi4mi <seyi4mi99@gmail.com>
2022-10-05 13:54:04 +0800

    created comp folder with contactForm file

--- frontend/pages/contact/index.js
+++ frontend/pages/contact/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from './contact.module.css';
+import abstyle from '../about/about.module.css';
 import styless from '../../styles/Home.module.css';
 import Image from 'next/image';
 
@@ -24,6 +25,11 @@ const Contact = () => {
       <Head>
         <title>BJF Properties | Contact</title>
       </Head>
+
+      <div className={abstyle.abtBanner}>
+        <h2 className={abstyle.bannerHeader}>CONTACT US</h2>
+        <p className={abstyle.bannersubH}>Let's Get In Touch </p>
+      </div>
 
       <div className={styles.contactsContainer}>
         <h3 className={styles.contactHeader}>Get To Know us More !</h3>
